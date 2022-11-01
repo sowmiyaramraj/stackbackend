68ef55b74f5e5e75e619924affdb5b8615c9b0a5
sowmiyaramraj <sowmiyaramraj.0505@gmail.com>
2022-11-01 17:29:13 +0530

    -

--- module/user.js
+++ module/user.js
@@ -1,57 +1,57 @@
-const mongo = require("../connect");
-const {ObjectId}= require("mongodb");
+// const mongo = require("../connect");
+// const {ObjectId}= require("mongodb");
 
-module.exports.getEmployees=async(req,res,next)=>{
-    try{
-        const getemployees= await mongo.selectedDb
-        .collection("employee")
-        .find().toArray();
-        res.send(getemployees);
-    }catch(err){
-        res.status(500).send(getemployees);
+// module.exports.getEmployees=async(req,res,next)=>{
+//     try{
+//         const getemployees= await mongo.selectedDb
+//         .collection("employee")
+//         .find().toArray();
+//         res.send(getemployees);
+//     }catch(err){
+//         res.status(500).send(getemployees);
 
-    }
+//     }
   
-};
+// };
 
-module.exports.updateEmployees=async(req,res,next)=>{
-  try{
-    const id=req.params.employeeid;
-  const updatedresponce=await mongo.selectedDb
-  .collection("employee")
-  .findOneAndUpdate({_id:ObjectId(id)},
-  {$set:{...req.body}},
- { returnDocument:"after"});
-  res.send(updatedresponce);
-}
-  catch(err){
-    console.error(err);
-    res.status(500).send(err);
-   }
-};
+// module.exports.updateEmployees=async(req,res,next)=>{
+//   try{
+//     const id=req.params.employeeid;
+//   const updatedresponce=await mongo.selectedDb
+//   .collection("employee")
+//   .findOneAndUpdate({_id:ObjectId(id)},
+//   {$set:{...req.body}},
+//  { returnDocument:"after"});
+//   res.send(updatedresponce);
+// }
+//   catch(err){
+//     console.error(err);
+//     res.status(500).send(err);
+//    }
+// };
 
-module.exports.createEmployees=async(req,res,next)=>{
-   try{
-   const insertedresponse=await mongo.selectedDb
-   .collection("employee")
-   .insertOne(req.body);
-   res.send(insertedresponse);
-   }
-   catch(err){
-    console.error(err);
-    res.status(500).send(err);
-   }
-};
+// module.exports.createEmployees=async(req,res,next)=>{
+//    try{
+//    const insertedresponse=await mongo.selectedDb
+//    .collection("employee")
+//    .insertOne(req.body);
+//    res.send(insertedresponse);
+//    }
+//    catch(err){
+//     console.error(err);
+//     res.status(500).send(err);
+//    }
+// };
 
-module.exports.deleteEmployees=async(req,res,next)=>{
-    try{
-        const id=req.params.id;
-        const deletedresponce=await mongo.selectedDb
-        .collection("employee")
-        .remove({_id:ObjectId(id)});
-        res.send(deletedresponce);
-    } catch(err){
-        console.error(err);
-        res.status(500).send(err);
-       }
-};
+// module.exports.deleteEmployees=async(req,res,next)=>{
+//     try{
+//         const id=req.params.id;
+//         const deletedresponce=await mongo.selectedDb
+//         .collection("employee")
+//         .remove({_id:ObjectId(id)});
+//         res.send(deletedresponce);
+//     } catch(err){
+//         console.error(err);
+//         res.status(500).send(err);
+//        }
+// };
--- router/employeerouter.js
+++ router/employeerouter.js
@@ -1,14 +1,14 @@
-const express=require("express");
-const router=express.Router();
-const employeeModule=require("../module/employee");
-const auth=require("../module/authmodule");
+// const express=require("express");
+// const router=express.Router();
+// const employeeModule=require("../module/employee");
+// const auth=require("../module/authmodule");
 
-router.get("/get",employeeModule.getEmployees);
+// router.get("/get",employeeModule.getEmployees);
 
-router.put("/update/:employeeid",auth.authorizeuser,employeeModule.updateEmployees);
+// router.put("/update/:employeeid",auth.authorizeuser,employeeModule.updateEmployees);
 
-router.delete("/delete/:id",auth.authorizeuser,employeeModule.deleteEmployees);
+// router.delete("/delete/:id",auth.authorizeuser,employeeModule.deleteEmployees);
 
-router.post("/create",auth.authorizeuser,employeeModule.createEmployees);
+// router.post("/create",auth.authorizeuser,employeeModule.createEmployees);
 
-module.exports=router;
+// module.exports=router;
